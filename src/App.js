fed01ef72c5a3c45778e57a4982f5a62a6f18ee8
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-04-21 13:43:52 +0300

    learning events

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+//working onClick, onChange, onSubmit react events
 function App() {
+  const handleClick=(event)=>{
+    event.preventDefault()
+    alert('did you just lick me!')
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>React events</h3>
+        <button onClick={handleClick}>Click me</button>
       </header>
     </div>
   );
